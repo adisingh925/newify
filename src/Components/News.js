0dac0c4702f602826d8725fe110528eeeea5eec8
aditya singh <adisingh925gmail.com>
2023-06-10 11:02:06 +0530

    initial commit

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner'
 
 export class News extends Component {
 
@@ -19,9 +20,7 @@ export class News extends Component {
     }
 
     nextButtonClick = () => {
-        if (this.state.page + 1 <= Math.ceil(this.state.totalItems / 20)) {
-            this.fetchData(1)
-        }
+        this.fetchData(1)
     }
 
     prevButtonClick = () => {
@@ -32,19 +31,21 @@ export class News extends Component {
 
     fetchData = async (operation) => {
         console.log(this.state.page)
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d56ab885e81741e191851cc013eb2c5f&page=${this.state.page + operation}&pagesize=20`
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d56ab885e81741e191851cc013eb2c5f&page=${this.state.page + operation}&pagesize=${this.props.pageSize}`
+        this.setState({ loading: true})
         let data = await fetch(url)
         let parsedData = await data.json()
         console.log(parsedData)
-        this.setState({ articles: parsedData.articles, totalItems: parsedData.totalResults, page: this.state.page + operation })
+        this.setState({ articles: parsedData.articles, totalItems: parsedData.totalResults, page: this.state.page + operation, loading : false })
     }
 
     render() {
         return (
             <div className='container my-3'>
                 <h2>Newsify - Top Headlines</h2>
+                {this.state.loading && <Spinner />}
                 <div className='row'>
-                    {this.state.articles.map((element) => {
+                    {!this.state.loading === true && this.state.articles.map((element) => {
                         return <div className='col-md-4' key={element.url}>
                             <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
@@ -52,7 +53,7 @@ export class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button type="button" disabled={this.state.page <= 1} onClick={this.prevButtonClick} className="btn btn-dark mx-3 my-3">&larr; Previous</button>
-                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalItems / 20)}  onClick={this.nextButtonClick} className="btn btn-dark mx-3 my-3">Next &rarr;</button>
+                    <button type="button" disabled={(this.state.page + 1) > Math.ceil(this.state.totalItems / this.props.pageSize)} onClick={this.nextButtonClick} className="btn btn-dark mx-3 my-3">Next &rarr;</button>
                 </div>
             </div>
         )
